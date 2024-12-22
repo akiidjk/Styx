591c714deed7d6525a6f2551ece6e5a66d5de275
akiidjk <dragonfaiver@gmail.com>
2024-12-22 19:49:01 +0100

    Add Go generate directives for eBPF counter and collector

---
+++ internal/ebpf/generated/gen.go
@@ -0,0 +1,4 @@
+package ebpf
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go counter ../ebpf_module_kernel/counter.c --pkg counterebpf
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go collecter ../ebpf_module_kernel/collect.c --pkg collectebpf
