62e81e7e3c5466ad77b303281bded6bd2c2d1de0
akiidjk <dragonfaiver@gmail.com>
2025-01-01 23:34:42 +0100

    Refactor packet handling in XDP program and improve logging for payload processing

--- internal/ebpf/ebpf_module_kernel/collect.c
+++ internal/ebpf/ebpf_module_kernel/collect.c
@@ -1,6 +1,11 @@
 // go:build ignore
 // +build ignore
 
+// The printk function is used to print messages to the kernel log. It is
+// similar to the printf function in C. `sudo cat
+// /sys/kernel/debug/tracing/trace_pipe`
+
+#include <asm-generic/int-ll64.h>
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
@@ -10,49 +15,79 @@
 #include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
+#include <stdlib.h>
 
-struct packet_info {
-  __u32 src_ip;
-  __u32 dst_ip;
-  __u16 protocol;
-  __u16 pad;
+#define MAX_PAYLOAD_SIZE 2048
+
+struct packet_s {
+  long payload_size;
+  unsigned char payload[MAX_PAYLOAD_SIZE];
 };
 
 struct {
   __uint(type, BPF_MAP_TYPE_RINGBUF);
   __uint(max_entries, 4096);
-} pkt_data SEC(".maps");
+} module_map SEC(".maps");
+
+struct {
+  __uint(type, BPF_MAP_TYPE_ARRAY);
+  __uint(max_entries, 1);
+  __type(key, __u32);
+  __type(value, _Bool);
+} control_map SEC(".maps");
 
 SEC("xdp")
 int share_data(struct xdp_md *ctx) {
-  __u32 key = 0;
-  struct packet_info info = {0};
 
-  // Get the packet data
   void *data = (void *)(long)ctx->data;
   void *data_end = (void *)(long)ctx->data_end;
 
-  // Some check
-  struct ethhdr *eth = data;
-  if ((void *)(eth + 1) > data_end)
+  if (data + sizeof(struct ethhdr) > data_end) {
     return XDP_PASS;
+  }
 
-  if (eth->h_proto != bpf_htons(ETH_P_IP))
+  // Calcola la dimensione del payload
+  long payload_size = data_end - data;
+  if (payload_size > MAX_PAYLOAD_SIZE) {
+    bpf_printk("Payload size too big: %ld\n", payload_size);
+    payload_size = MAX_PAYLOAD_SIZE;
+  }
+
+  struct packet_s *packet =
+      bpf_ringbuf_reserve(&module_map, sizeof(struct packet_s), 0);
+  if (!packet) {
     return XDP_PASS;
+  }
 
-  struct iphdr *ip = (void *)(eth + 1);
-  if ((void *)(ip + 1) > data_end)
+  packet->payload_size = payload_size;
+
+  unsigned char *src = data;
+  for (long i = 0; i < payload_size; i++) {
+    if (src + i >= data_end) {
+      bpf_ringbuf_discard(packet, 0);
+      return XDP_ABORTED;
+    }
+    packet->payload[i] = src[i];
+  }
+
+  bpf_ringbuf_submit(packet, 0);
+
+  bpf_printk("Packet processed\n");
+
+  __u32 key = 0;
+  _Bool *control_value = bpf_map_lookup_elem(&control_map, &key);
+  if (control_value) {
+    if (*control_value) {
+      bpf_printk("Passing package\n");
+      return XDP_PASS;
+    } else {
+      bpf_printk("Dropping package\n");
+      return XDP_DROP;
+    }
+  } else {
+    bpf_printk("Failed to retrieve control value from map\n");
     return XDP_PASS;
-
-  // Fill the packet info
-
-  info.src_ip = ip->saddr;
-  info.dst_ip = ip->daddr;
-  info.protocol = ip->protocol;
-
-  bpf_ringbuf_output(&pkt_data, &info, sizeof(info), 0);
-
-  return XDP_PASS;
+  }
 }
 
 char __license[] SEC("license") = "Dual MIT/GPL";
