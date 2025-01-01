3e8d412af3a18cf4ce174f4176e2437827f5fe5c
akiidjk <dragonfaiver@gmail.com>
2025-01-01 23:34:45 +0100

    Refactor packet processing to use gopacket for layer decoding and improve control map updates

--- internal/ebpf/ebpf_module_user/collect.go
+++ internal/ebpf/ebpf_module_user/collect.go
@@ -10,15 +10,57 @@ import (
 	ebpfModules "github.com/akiidjk/styx/internal/ebpf/generated"
 	"github.com/akiidjk/styx/internal/utils"
 	"github.com/akiidjk/styx/internal/utils/logger"
+	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/ringbuf"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 )
 
 // Set the value in the struct public or the program crash
-type packetInfo struct {
-	SrcIP    uint32
-	DstIP    uint32
-	Protocol uint16
-	_        uint16
+type Packet struct {
+	Payload []byte
+	Size    int64
+}
+
+func processPacket(packetPayload []byte) bool {
+	var eth layers.Ethernet
+	var ip4 layers.IPv4
+	var ip6 layers.IPv6
+	var tcp layers.TCP
+
+	parser := gopacket.NewDecodingLayerParser(
+		layers.LayerTypeEthernet,
+		&eth, &ip4, &ip6, &tcp,
+	)
+	decoded := []gopacket.LayerType{}
+
+	if err := parser.DecodeLayers(packetPayload, &decoded); err != nil {
+		logger.Warningf("Could not decode layers: %v", err)
+		return true
+	}
+
+	for _, layerType := range decoded {
+		switch layerType {
+		case layers.LayerTypeEthernet:
+			logger.Debug("Ethernet Layer:")
+			logger.Debugf("    Src MAC: %s, Dst MAC: %s", eth.SrcMAC, eth.DstMAC)
+		case layers.LayerTypeIPv4:
+			logger.Debug("IPv4 Layer:")
+			logger.Debugf("    Src IP: %s, Dst IP: %s", ip4.SrcIP, ip4.DstIP)
+			logger.Debug(fmt.Sprint(ip4.SrcIP))
+			logger.Debug(fmt.Sprint(ip4.SrcIP) == "192.168.1.20")
+			if fmt.Sprint(ip4.SrcIP) == "192.168.1.20" {
+				return false
+			}
+		case layers.LayerTypeIPv6:
+			logger.Debug("IPv6 Layer:")
+			logger.Debugf("    Src IP: %s, Dst IP: %s", ip6.SrcIP, ip6.DstIP)
+		case layers.LayerTypeTCP:
+			logger.Debug("TCP Layer:")
+			logger.Debugf("    Src Port: %d, Dst Port: %d", tcp.SrcPort, tcp.DstPort)
+		}
+	}
+	return true
 }
 
 func Collect(ifname string) {
@@ -33,10 +75,14 @@ func Collect(ifname string) {
 
 	logger.Info("Sharing incoming packets on ", ifname)
 
+	controlMap := objs.ControlMap
+	keyControlMap := uint32(0)
+	var valueControlMap bool = true
+
 	stop := make(chan os.Signal, 5)
 	signal.Notify(stop, os.Interrupt)
 
-	rd, err := ringbuf.NewReader(objs.PktData)
+	rd, err := ringbuf.NewReader(objs.ModuleMap)
 	if err != nil {
 		logger.Fatal("Failed to open ringbuf reader: ", err)
 		os.Exit(1)
@@ -61,15 +107,30 @@ func Collect(ifname string) {
 			continue
 		}
 
-		var data packetInfo
-		if err := binary.Read(bytes.NewReader(record.RawSample), binary.LittleEndian, &data); err != nil {
-			logger.Warning("Error decoding data: ", err)
+		reader := bytes.NewReader(record.RawSample)
+
+		var size int64
+		if err := binary.Read(reader, binary.LittleEndian, &size); err != nil {
+			logger.Warning("Error reading size: ", err)
 			continue
 		}
 
-		logger.Info(fmt.Sprintf("SrcIP: %d.%d.%d.%d, DstIP: %d.%d.%d.%d, Protocol: %d",
-			data.SrcIP>>24, (data.SrcIP>>16)&0xFF, (data.SrcIP>>8)&0xFF, data.SrcIP&0xFF,
-			data.DstIP>>24, (data.DstIP>>16)&0xFF, (data.DstIP>>8)&0xFF, data.DstIP&0xFF,
-			data.Protocol))
+		payload := make([]byte, size)
+		if _, err := reader.Read(payload); err != nil {
+			logger.Warning("Error reading payload: ", err)
+			continue
+		}
+
+		packet := Packet{
+			Size:    size,
+			Payload: payload,
+		}
+
+		valueControlMap = processPacket(packet.Payload)
+		if err := controlMap.Update(keyControlMap, valueControlMap, ebpf.UpdateAny); err != nil {
+			logger.Fatal("Failed to update control map: ", err)
+		}
+
+		// logger.Debug(packetDecoded.Dump())
 	}
 }
