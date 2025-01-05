973da1483c2d01b2035984dc6a4601426ec4cc3b
akiidjk <dragonfaiver@gmail.com>
2025-01-05 13:20:44 +0100

    rename ebpf package to ebpfGenerated for improved clarity

--- internal/ebpf/generated/gen.go
+++ internal/ebpf/generated/gen.go
@@ -1,4 +1,4 @@
-package ebpf
+package ebpfGenerated
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go counter ../ebpf_module_kernel/counter.c --pkg counterebpf
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go collecter ../ebpf_module_kernel/collect.c --pkg collectebpf
