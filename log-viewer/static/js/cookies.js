c15bbb9ee70534354c813d423e87f48caecb4237
akiidjk <dragonfaiver@gmail.com>
2025-01-10 18:20:23 +0100

    refactor: update cookie handling functions for consistency and clarity

--- log-viewer/static/js/cookies.js
+++ log-viewer/static/js/cookies.js
@@ -1,17 +1,18 @@
 function loadPreferences() {
-  const fileName = Cookies.get('logFileName');
-  const filterValue = Cookies.get('filterValue');
-  const pageLength = Cookies.get('pageLength');
-  return { fileName, filterValue, pageLength };
+  const fileName = Cookies.get("logFileName");
+  const filterValue = Cookies.get("filterValue");
+  const pageSize = Cookies.get("pageSize");
+  return { fileName, filterValue, pageLength: pageSize };
 }
 
-function savePreferences(fileName) {
-  Cookies.set('logFileName', fileName, { expires: 7 });
+function saveFilename(fileName) {
+  Cookies.set("logFileName", fileName, { expires: 7 });
 }
 
+function saveFilterValue(filterValue) {
+  Cookies.set("filterValue", filterValue, { expires: 7 });
+}
 
-function savePageLength(settings) {
-  const api = this.api();
-  const pageInfo = api.page.info();
-  Cookies.set('pageLength', pageInfo.length, { expires: 7 });
+function savePageLength(size) {
+  Cookies.set("pageSize", size, { expires: 7 });
 }
