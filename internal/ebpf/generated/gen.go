244e6f5fe85c97042e5473f0e1cfe8c3e8ca2849
akiidjk <dragonfaiver@gmail.com>
2025-01-11 23:23:23 +0100

    refactor: update eBPF generation command to filter IP addresses

--- internal/ebpf/generated/gen.go
+++ internal/ebpf/generated/gen.go
@@ -1,4 +1,4 @@
 package ebpfGenerated
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go counter ../ebpf_module_kernel/counter.c --pkg counterebpf
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go collecter ../ebpf_module_kernel/collect.c --pkg collectebpf
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go filterip ../ebpf_module_kernel/filter_ip.c --pkg filterip
