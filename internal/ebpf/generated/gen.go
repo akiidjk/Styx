af72b9455647516c2089818f5ef01a1abdfa2e6c
akiidjk <dragonfaiver@gmail.com>
2025-01-16 22:56:06 +0100

    fix: update go:generate commands to reference .ebpf.c files

--- internal/ebpf/generated/gen.go
+++ internal/ebpf/generated/gen.go
@@ -1,6 +1,6 @@
 package ebpfGenerated
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go counter ../ebpf_module_kernel/counter.c --pkg counterebpf
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go filterip ../ebpf_module_kernel/filter_ip.c --pkg filterip
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go filtermac ../ebpf_module_kernel/filter_mac.c --pkg filtermac
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go dispacher ../ebpf_module_kernel/dispacher.c --pkg dispacher
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go counter ../ebpf_module_kernel/counter.ebpf.c --pkg counterebpf
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go filterip ../ebpf_module_kernel/filter_ip.ebpf.c --pkg filterip
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go filtermac ../ebpf_module_kernel/filter_mac.ebpf.c --pkg filtermac
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go dispacher ../ebpf_module_kernel/dispacher.ebpf.c --pkg dispacher
