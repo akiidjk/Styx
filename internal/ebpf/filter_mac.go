c1b8e6191355f4ac3f51be72cea7ea6f5d0724ec
akiidjk <dragonfaiver@gmail.com>
2025-01-16 22:55:24 +0100

    refactor: simplify loadEBPFObjectsFilterMac function and remove redundant error handling

--- internal/ebpf/filter_mac.go
+++ internal/ebpf/filter_mac.go
@@ -1,49 +1,20 @@
 package ebpf
 
 import (
-	"log"
-	"time"
-
 	ebpfGenerated "github.com/akiidjk/styx/internal/ebpf/generated"
-	"github.com/akiidjk/styx/internal/utils"
 )
 
-func loadEBPFObjectsFilterMac() (*ebpfGenerated.FiltermacObjects, error) {
+func loadEBPFObjectsFilterMac() *ebpfGenerated.FiltermacObjects {
 	var objs ebpfGenerated.FiltermacObjects
 	if err := ebpfGenerated.LoadFiltermacObjects(&objs, nil); err != nil {
-		return nil, err
+		logger.Fatal().Err(err).Msg("Failed to load eBPF objects")
 	}
-	return &objs, nil
+	return &objs
 }
 
 func RunPacketFilterMac(ifname string, blockedMacs []string) {
-	ebpfObjects, err := loadEBPFObjectsFilterMac()
-	if err != nil {
-		logger.Fatal().Err(err).Msg("Failed to load eBPF objects")
-	}
+	ebpfObjects := loadEBPFObjectsFilterMac()
 	defer ebpfObjects.Close()
-
-	start := time.Now()
-	log.Println("ATTACO")
-	link := utils.LinkInterface(ifname, ebpfObjects.XdpFilterMac)
-	log.Println("STACCO")
-	link.Close()
-	elapsed := time.Since(start).Milliseconds()
-	log.Printf("Tempo trascorso: %d ms\n", elapsed)
-
-	log.Println("ATACCO")
-	link = utils.LinkInterface(ifname, ebpfObjects.XdpFilterMac)
-	log.Println("STACCO")
-	link.Close()
-	elapsed = time.Since(start).Milliseconds()
-	log.Printf("Tempo trascorso: %d ms\n", elapsed)
-
-	log.Println("ATTACO")
-	link = utils.LinkInterface(ifname, ebpfObjects.XdpFilterMac)
-	log.Println("STACCO")
-	link.Close()
-	elapsed = time.Since(start).Milliseconds()
-	log.Printf("Tempo trascorso: %d ms\n", elapsed)
 
 	logger.Info().Str("Interface", ifname).Msg("Starting packet filter")
 	for {
