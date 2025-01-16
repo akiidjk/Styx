2f1f2f22bbc0e70169a93762db2ad157df1a9343
akiidjk <dragonfaiver@gmail.com>
2025-01-16 22:55:08 +0100

    feat: refactor root command execution and add logging for better traceability

--- cmd/root.go
+++ cmd/root.go
@@ -4,10 +4,9 @@ Copyright © 2024 Akiidjk akiidjk@proton.me
 package cmd
 
 import (
-	"os"
-
 	"github.com/akiidjk/styx/internal/ebpf"
 	"github.com/akiidjk/styx/internal/utils"
+	"github.com/akiidjk/styx/internal/utils/logger"
 	"github.com/spf13/cobra"
 )
 
@@ -24,25 +23,32 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// go ebpf.RunPacketFilterMac(ifname, blockMacs)
-		// go ebpf.RunPacketFilterIP(ifname, blockIPs)
-
-		go ebpf.Dispach(ifname)
-		utils.HandleTerminate()
-	},
+	Run: RunFirewall,
 }
+
+var l = logger.GetLogger()
 
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
-		os.Exit(1)
+		l.Fatal().Err(err).Msg("Failed to execute root command")
 	}
 }
 
 func init() {
+	l.Info().Msg("Starting Styx")
 	rootCmd.PersistentFlags().StringVarP(&ifname, "ifname", "i", "", "Interface to be observed")
 	rootCmd.MarkFlagRequired("ifname")
 	rootCmd.PersistentFlags().StringSliceVarP(&blockIPs, "block-ips", "", []string{}, "List of IPs address to block")
 	rootCmd.PersistentFlags().StringSliceVarP(&blockMacs, "block-macs", "", []string{}, "List of MACs address to block")
+	l.Debug().Msg("Root command initialized")
+}
+
+func RunFirewall(cmd *cobra.Command, args []string) {
+	l.Info().Msg("Loading eBPF objects")
+	ebpf.LoadObjectsEBPF()
+	defer ebpf.CloseAllObject()
+
+	go ebpf.Dispach(ifname)
+	utils.HandleTerminate()
 }
