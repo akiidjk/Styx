ba5d1152acaf9abbcf863d04ba2d67440a38759c
akiidjk <dragonfaiver@gmail.com>
2025-01-17 19:15:32 +0100

    feat: implement IP address conversion functions for decimal and string formats

--- tests/test.go
+++ tests/test.go
@@ -1,80 +1,46 @@
 package main
 
 import (
-	"io"
-	"log"
-	"os"
-	"path/filepath"
-	"strconv"
-	"time"
-
-	"github.com/rs/zerolog"
+	"fmt"
+	"net"
 )
 
-type Colors struct {
-	Reset   string
-	Red     string
-	Green   string
-	Yellow  string
-	Blue    string
-	Magenta string
-	Cyan    string
-	White   string
-}
-
-var DefaultColors = Colors{
-	Reset:   "\033[0m",
-	Red:     "\033[31m",
-	Green:   "\033[32m",
-	Yellow:  "\033[33m",
-	Blue:    "\033[34m",
-	Magenta: "\033[35m",
-	Cyan:    "\033[36m",
-	White:   "\033[37m",
-}
-
-// Possible levels:
-// panic (zerolog.PanicLevel, 5)
-// fatal (zerolog.FatalLevel, 4)
-// error (zerolog.ErrorLevel, 3)
-// warn (zerolog.WarnLevel, 2)
-// info (zerolog.InfoLevel, 1)
-// debug (zerolog.DebugLevel, 0)
-// trace (zerolog.TraceLevel, -1)
-
-var logger zerolog.Logger
-var logFilename string = time.Now().Local().Format("2006-01-02_15-04-05") + ".log"
-var logFile *os.File
-
-const logDir string = "styx"
-
-func init() {
-
-	eiud := os.Geteuid()
-	if eiud != 0 {
-		log.Fatal("Remember to run the program with `sudo` or with root")
-		os.Exit(1)
+func IpToDecimal(ipStr string) (uint32, error) {
+	if ipStr == "" {
+		return 0, fmt.Errorf("empty IP address")
 	}
 
-	newpath := filepath.Join("/", "var", "log", logDir)
-	err := os.MkdirAll(newpath, os.ModePerm)
-	pathLogs := filepath.Join(newpath, logFilename)
-
-	logFile, err := os.OpenFile(pathLogs, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		log.Fatal(err)
+	ip := net.ParseIP(ipStr)
+	if ip == nil {
+		return 0, fmt.Errorf("invalid IP address: %s", ipStr)
 	}
 
-	// Some customizations
-	zerolog.CallerMarshalFunc = func(pc uintptr, file string, line int) string {
-		return filepath.Base(file) + ":" + strconv.Itoa(line)
+	ip4 := ip.To4()
+	if ip4 == nil {
+		return 0, fmt.Errorf("not an IPv4 address: %s", ipStr)
 	}
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	logger = zerolog.New(io.Writer(logFile)).With().Caller().Timestamp().Logger()
+	result := uint32(ip4[3])<<24 | uint32(ip4[2])<<16 | uint32(ip4[1])<<8 | uint32(ip4[0])
 
+	return result, nil
 }
 
-func SetLevel(level zerolog.Level) {
-	zerolog.SetGlobalLevel(level)
+func NumberToIp(number uint32) (string, error) {
+	ip := net.IPv4(
+		byte(number>>24),
+		byte(number>>16),
+		byte(number>>8),
+		byte(number),
+	)
+
+	result := ip.String()
+
+	return result, nil
+}
+
+func main() {
+	number, _ := IpToDecimal("192.168.1.1")
+	fmt.Println(number)
+	ip, _ := NumberToIp(number)
+	fmt.Println(ip)
 }
