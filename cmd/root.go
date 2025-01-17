79f02ad9ece3e4001af08d4dc099e1dda411d4f2
akiidjk <dragonfaiver@gmail.com>
2025-01-17 19:14:40 +0100

    refactor: replace RunFirewall function with firewall.Run in root command

--- cmd/root.go
+++ cmd/root.go
@@ -4,8 +4,7 @@ Copyright © 2024 Akiidjk akiidjk@proton.me
 package cmd
 
 import (
-	"github.com/akiidjk/styx/internal/ebpf"
-	"github.com/akiidjk/styx/internal/utils"
+	"github.com/akiidjk/styx/internal/firewall"
 	"github.com/akiidjk/styx/internal/utils/logger"
 	"github.com/spf13/cobra"
 )
@@ -23,7 +22,7 @@ examples and usage of using your application. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: RunFirewall,
+	Run: firewall.Run,
 }
 
 var l = logger.GetLogger()
@@ -42,13 +41,4 @@ func init() {
 	rootCmd.PersistentFlags().StringSliceVarP(&blockIPs, "block-ips", "", []string{}, "List of IPs address to block")
 	rootCmd.PersistentFlags().StringSliceVarP(&blockMacs, "block-macs", "", []string{}, "List of MACs address to block")
 	l.Debug().Msg("Root command initialized")
-}
-
-func RunFirewall(cmd *cobra.Command, args []string) {
-	l.Info().Msg("Loading eBPF objects")
-	ebpf.LoadObjectsEBPF()
-	defer ebpf.CloseAllObject()
-
-	go ebpf.Dispach(ifname)
-	utils.HandleTerminate()
 }
