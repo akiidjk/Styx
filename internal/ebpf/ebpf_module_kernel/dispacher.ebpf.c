723918aa988cec5566113ae642126e90ef8129f4
akiidjk <dragonfaiver@gmail.com>
2025-01-17 19:14:55 +0100

    feat: add event_output map for enhanced event handling in xdp_dispach

--- internal/ebpf/ebpf_module_kernel/dispacher.ebpf.c
+++ internal/ebpf/ebpf_module_kernel/dispacher.ebpf.c
@@ -8,13 +8,18 @@ struct {
   __uint(max_entries, 2);
   __uint(key_size, sizeof(__u32));
   __uint(value_size, sizeof(__u32));
+  __uint(pinning, LIBBPF_PIN_BY_NAME);
 } prog_array SEC(".maps");
+
+struct {
+  __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+  __uint(max_entries, 16);
+  __uint(pinning, LIBBPF_PIN_BY_NAME);
+} event_output SEC(".maps");
 
 SEC("xdp")
 int xdp_dispach(struct xdp_md *ctx) {
-  bpf_printk("DISPACHING\n");
   bpf_tail_call(ctx, &prog_array, 0);
-  bpf_printk("Tail call failed\n");
   return XDP_PASS;
 }
 char __license[] SEC("license") = "Dual MIT/GPL";
