b45bca548f871a0d2f78905a3dd1f5fd5f712710
akiidjk <dragonfaiver@gmail.com>
2025-01-17 19:15:08 +0100

    feat: add IP address conversion functions and improve error handling

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,65 +2,67 @@ package utils
 
 import (
 	"fmt"
-	"math"
 	"net"
 	"os"
 	"os/signal"
 	"slices"
-	"strconv"
 	"strings"
 )
 
-func ReverseString(str string) (result string) {
-	for _, v := range str {
-		result = string(v) + result
+func Ipv4ToDecimal(ipStr string) (uint32, error) {
+	if ipStr == "" {
+		return 0, fmt.Errorf("empty IP address")
 	}
-	return
-}
 
-func IpToDecimal(ipStr string) (uint32, error) {
 	ip := net.ParseIP(ipStr)
 	if ip == nil {
-		return 0, fmt.Errorf("Ip not valid: %s", ipStr)
-	}
-	ip = ip.To4()
-	ipString := ip.String()
-	if ip == nil {
-		return 0, fmt.Errorf("The ip is not IPv4: %s", ipStr)
+		return 0, fmt.Errorf("invalid IP address: %s", ipStr)
 	}
 
-	octets := strings.Split(ipString, ".")
-	slices.Reverse(octets)
-	var finalIpString string
-	for i := 0; i < 4; i++ {
-		int_octet, _ := strconv.Atoi(octets[i])
-		octets[i] = strconv.FormatInt(int64(int_octet), 2)
-		octets[i] = strings.Repeat("0", 8-len(octets[i])) + octets[i]
-		finalIpString += octets[i]
+	ip4 := ip.To4()
+	if ip4 == nil {
+		return 0, fmt.Errorf("not an IPv4 address: %s", ipStr)
 	}
 
-	logger.Debug().Str("IP", finalIpString).Msg("Converting IP to binary")
-	ip_interger := BinaryToDec(finalIpString)
-	logger.Debug().Uint32("IP", uint32(ip_interger)).Msg("Converted IP")
+	result := uint32(ip4[3])<<24 | uint32(ip4[2])<<16 | uint32(ip4[1])<<8 | uint32(ip4[0])
 
-	return uint32(ip_interger), nil
+	logger.Debug().
+		Str("input_ip", ipStr).
+		Uint32("decimal", result).
+		Msg("IP converted to decimal")
+
+	return result, nil
 }
 
-func BinaryToDec(binary_string string) uint32 {
-	value := uint32(0)
-	length := len(binary_string)
-	if length != 32 {
-		return 0
+func NumberToIpv4(number uint32) (string, error) {
+	ip := net.IPv4(
+		byte(number>>24),
+		byte(number>>16),
+		byte(number>>8),
+		byte(number),
+	)
+
+	result := ip.String()
+
+	logger.Debug().
+		Uint32("input_number", number).
+		Str("ip", result).
+		Msg("Number converted to IP")
+
+	return result, nil
+}
+
+/*
+Function for do the reverse of the IP address (only visualize purpose)
+*/
+func ReverseIpv4(ipv4 string) string {
+	if net.ParseIP(ipv4) == nil {
+		return "Invalid IP"
 	}
-	for i := 0; i < len(binary_string); i++ {
-		if binary_string[i] != '0' && binary_string[i] != '1' {
-			return 0
-		}
-		if binary_string[i] == '1' {
-			value += uint32(math.Pow(2, float64(length-i-1)))
-		}
-	}
-	return value
+	octets := strings.Split(ipv4, ".")
+	slices.Reverse(octets)
+	ipv4 = strings.Join(octets, ".")
+	return ipv4
 }
 
 func HandleTerminate() {
