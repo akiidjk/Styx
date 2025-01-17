9ac23090991e8bcf9a4d23b9484c786222645711
akiidjk <dragonfaiver@gmail.com>
2025-01-17 19:14:44 +0100

    feat: enhance Dispach function with pinned map loading and improved logging

--- internal/ebpf/dispacher.go
+++ internal/ebpf/dispacher.go
@@ -6,12 +6,22 @@ import (
 	"github.com/cilium/ebpf"
 )
 
+const pinPathProgArray string = "/sys/fs/bpf/prog_array"
+const pinPathEventOutputMap string = "/sys/fs/bpf/event_output"
+
+var objects = []string{"FilterIp", "FilterMac"}
+
 /*
 Load the eBPF objects for the dispacher program
 */
 func LoadEBPFObjectsDispacher() *ebpfGenerated.DispacherObjects {
 	var objs ebpfGenerated.DispacherObjects
-	if err := ebpfGenerated.LoadDispacherObjects(&objs, nil); err != nil {
+	opts := ebpf.CollectionOptions{
+		Maps: ebpf.MapOptions{
+			PinPath: "/sys/fs/bpf",
+		},
+	}
+	if err := ebpfGenerated.LoadDispacherObjects(&objs, &opts); err != nil {
 		logger.Fatal().Err(err).Msg("Failed to load eBPF objects")
 	}
 	return &objs
@@ -29,15 +39,53 @@ func addPrograms(programs []*ebpf.Program, programMap *ebpf.Map) {
 	}
 }
 
-func Dispach(ifname string) {
+func Dispach(ifname string, blockIps []string) {
+	logger.Info().Msgf("Starting Dispach function for interface: %s", ifname)
 	dispacherObj, err := GetObject[*ebpfGenerated.DispacherObjects]("Dispacher")
 	if err != nil {
-		logger.Fatal().Err(err).Msg("Failed to get Dispacher object")
+		logger.Fatal().Err(err).Msgf("Failed to get Dispacher object for interface %s", ifname)
 	}
-	addPrograms(GetPrograms(), (*dispacherObj).ProgArray)
-	link := utils.LinkInterface(ifname, (*dispacherObj).XdpDispach)
-	defer link.Close()
+	logger.Info().Msg("Successfully obtained Dispacher object")
 
+	logger.Info().Msgf("Linking interface %s with XDP program", ifname)
+	link := utils.LinkInterface(ifname, (*dispacherObj).XdpDispach)
+	defer func() {
+		logger.Info().Msg("Closing interface link")
+		link.Close()
+	}()
+	logger.Info().Msg("Interface successfully linked")
+
+	// Load pinned map
+	ProgArray, err := ebpf.LoadPinnedMap(pinPathProgArray, nil)
+	if err != nil {
+		logger.Fatal().Err(err).Msgf("Failed to load pinned map from path: %s", pinPathProgArray)
+	}
+	logger.Info().Msgf("Successfully loaded pinned map from: %s", pinPathProgArray)
+	defer func() {
+		logger.Info().Msg("Closing pinned map")
+		ProgArray.Close()
+	}()
+
+	EventOutputMap, err := ebpf.LoadPinnedMap(pinPathEventOutputMap, nil)
+	if err != nil {
+		logger.Fatal().Err(err).Msgf("Failed to load pinned map from path: %s", EventOutputMap)
+	}
+	logger.Info().Msgf("Successfully loaded pinned map from: %s", EventOutputMap)
+	defer func() {
+		logger.Info().Msg("Closing pinned map")
+		EventOutputMap.Close()
+	}()
+
+	LoadObjectsEBPF(objects)
+
+	logger.Info().Msg("Adding programs to map...")
+	addPrograms(GetPrograms(), ProgArray)
+	logger.Info().Msg("Programs successfully added to map")
+
+	logger.Info().Msg("Entering infinite loop to keep program running")
+
+	RunPacketFilterIP(ifname, blockIps)
 	for {
+		// time.Sleep(time.Second)
 	}
 }
